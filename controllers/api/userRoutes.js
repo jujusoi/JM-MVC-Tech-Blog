124870db58e3bf078c03e7b3d27d8f5e5a655591
jujusoi <jalxmcdonald@hotmail.com>
2023-09-13 23:23:51 +0930

    started data route. need to seed db first

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,15 @@
+const userRoute = require('express').Router();
+const { User, Post, Comment } = require('../../models/');
+
+userRoute.get('/:id', async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.params.id, {
+            include: [{ model: Post }],
+        });
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(`Could not retrieve data`);
+    }
+});
+
+module.exports = userRoute;
--- controllers/index.js
+++ controllers/index.js
@@ -3,10 +3,13 @@ const homeRoute = require('./homerouter');
 const signUp = require('./signupRouter');
 const dashboardRoute = require('./dashboardRouter');
 const loginRoute = require('./loginRoute');
+const api = require('./api');
 
 router.use('/home', homeRoute);
 router.use('/sign-up', signUp);
 router.use('/dashboard', dashboardRoute);
-router.use('/login', signUp);
+router.use('/login', loginRoute);
+
+router.use('/api', api);
 
 module.exports = router;
