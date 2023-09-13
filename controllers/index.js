2a9eb7c9075357f3eec574919abf0dcc5cd03c0c
jujusoi <jalxmcdonald@hotmail.com>
2023-09-13 13:38:53 +0930

    added html js for later and rendered first handlebars. fixed static css

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router.get('/', async (req, res) => {
     try {
-        res.status(500).json(`Working`)
+        res.status(500).render(`signup`);
     } catch (err) {
         res.status(404).json(`Aint work`)
     }
