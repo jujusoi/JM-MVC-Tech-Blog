773783bea52e7bddecbf0d4e2a9b06842de41a47
jujusoi <jalxmcdonald@hotmail.com>
2023-09-16 10:02:44 +0930

    added dashboard functionality. will now render ashboard with all session user posts, and show a special message if there is none.

--- controllers/dashboardRouter.js
+++ controllers/dashboardRouter.js
@@ -1,8 +1,27 @@
 const dashboard = require('express').Router();
+const { User, Post } = require('../models');
 
 dashboard.get('/',  async (req, res) => {
     try {
-        res.status(200).render(`dashboard`);
+        const userPosts = await Post.findAll({
+            where: {
+                user_id: req.session.user.user_id,
+            },
+            include: {
+                model: User, attributes: {
+                    exclude: [ 'password' ],
+                },
+            },
+        });
+        console.log(userPosts);
+        if (userPosts.length === 0) {
+            res.status(200).render(`dashboard`);
+        } else {
+            const mappedPost = userPosts.map((post) => post.get({ plain: true }));
+            res.status(200).render(`dashboard`, {
+                mappedPost,
+            });
+        }
     } catch (err) {
         res.status(404).json(`Not found`);
     }
